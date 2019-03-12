eafdba668c8ee7393ef3b2636a2b3243aa8051ce
ninni426 <akanksha26493@gmail.com>
2019-03-12 20:30:18 +0530

    Add files via upload

--- app/components/story-slide.js
+++ app/components/story-slide.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
-
+const {$} = Ember;
+const document = window.document;
 export default Component.extend({
     init(){
         this._super();
@@ -24,12 +25,24 @@ export default Component.extend({
          * Also pauses the GIF from display.
          * @param {Object} - story - GIF data from  
          */
-        stopAutoPlay( story ) {
-            console.log(story);
+        stopAutoPlay( story) {
             this.toggleProperty('carousel_pause');
-            console.log(this.get('carousel_pause'));
+            console.log(story);
             const control_value = this.get('carousel_pause') ? 'pause' : 'cycle';
             $('#carouselExampleControls').carousel(control_value);
+            // const myVideo = document.getElementById('gifID'); 
+            // console.log('myVideo before');
+            // console.log(myVideo);
+            // if (myVideo.paused) {
+            //     myVideo.play(); 
+            //     $('#carouselExampleControls').carousel('cycle');
+            // } else {
+            //     myVideo.pause();
+            //     $('#carouselExampleControls').carousel('pause');
+            // }
+
+            // console.log('myVideo after');
+            // console.log(myVideo);
         }
     }
 });
--- app/routes/stories.js
+++ app/routes/stories.js
@@ -1,14 +1,17 @@
+import Ember from 'ember';
 import Route from '@ember/routing/route';
-const {run, $, RSVP} = Ember;
+const {run, $} = Ember;
 
 export default Route.extend({
 
     model() {
-        console.log('model');
+        // $( '#refresh_bmp_modal' ).modal('show');
+		// $( '#refresh_bmp_modal' ).modal({
+		// 	backdrop: 'static',
+		// 	keyboard: false
+		// });
         const giffy_url = 'http://api.giphy.com/v1/gifs/trending?api_key=HavydXS9H2lPO3UOYtqf6SRT7C6sD0zQ';
-        console.log(giffy_url); 
-
-        return new Promise ((response, reject)=> {
+        return new Promise ( (response, reject) => {
             $.ajax({ 
             url: giffy_url,
             type: 'GET',
@@ -17,33 +20,34 @@ export default Route.extend({
             context: this,
             crossDomain: true,
             timeout: 60000
-        })
-        .done( resp => {
-            run( () => {
-                console.log(resp);
-                this.set('stories_data', resp.data);
-                response(resp);
+            })
+            .done( resp => {
+                run( () => {
+                    this.set('stories_data', resp.data);
+                    response(resp);
+                });
+            })
+            .fail( resp => {
+                run( () => {
+                    console.log('throw error ' + resp); 
+                    reject(resp);            
+                });
+            })
+            .always( () => {
+                run( () => {
+                    console.log('if spinner close it');         
+                });
             });
-        })
-        .fail( resp => {
-            run(() => {
-                console.log('throw error ' + resp); 
-                reject(resp);            
-            });
-        })
-        .always(()=> {
-            run(() => {
-                console.log('if spinner close it');         
-            });
-        });
         });
     },
       
-    afterModel(data) {
-        this.set('stories_data',  !( Ember.isEmpty(data.data)) );
+    afterModel(response) {
+        this.set('stories_data',  !( Ember.isEmpty(response.data)) );
     },
 
 	renderTemplate() {
+        // Remove loading spinner and display last fetch timestamp
+		// Ember.$('#refresh_bmp_modal').modal('hide');
 		if( this.get('stories_data') ) {
 			return this.render();
 		} else {
